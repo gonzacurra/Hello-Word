96309339a76f9bf104261d535a747551b5112579
gonzacurra <gonza_curra@hotmail.com>
2016-04-07 00:28:44 -0300

    Added files via upload

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+/* El proyecto aqui presentado es un hello word que efectua la salida por pantalla del texto "Hello Word!"
+a modo de demostracion de que el compilador usado funciona
+* Gonzalo Nicolas Curra
+* 6-04-16 ; 11:34
+*/
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
